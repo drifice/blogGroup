43f02b3980bdea55c9b49b399eae720e0e2d209e
Drifice <d.rhorbal59@gmail.com>
2022-01-28 14:17:33 +0100

    routes

--- Blog/src/app/public/public-routing.module.ts
+++ Blog/src/app/public/public-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './component/signin/signin.component';
 import { ArticleComponent } from './component/article/article.component';
 import { CommentaireComponent } from './component/commentaire/commentaire.component';
+import { TagComponent } from './component/tag/tag.component';
+import { CategorieComponent } from './component/categorie/categorie.component';
+import { AdduserComponent } from './component/adduser/adduser.component';
 
 
 const routes: Routes = [
@@ -17,6 +20,18 @@ const routes: Routes = [
   {
     path :'commentaire',
     component :CommentaireComponent,
+  },
+  {
+    path :'tag',
+    component :TagComponent,
+  },
+  {
+    path :'categorie',
+    component :CategorieComponent,
+  },
+  {
+    path :'addUser',
+    component :AdduserComponent,
   }
 ];
 
